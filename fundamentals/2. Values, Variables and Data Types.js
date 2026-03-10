2758fc2e05ec51b92c86f758818eb56b4b2f41cf
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-03-10 11:12:50 +0530

    values, variables and data types

---
+++ fundamentals/2. Values, Variables and Data Types.js
@@ -0,0 +1,21 @@
+// Values
+10;
+('Hello');
+true;
+null;
+undefined;
+
+// Variables
+let age = 20;
+
+// Primitive Data Types
+let height = 150; // Number
+let name = 'Himanshu'; // String
+let isStudent = true; // Boolean
+let someone; // Undefined
+let nullMe = null; // Null
+
+// Reference Types
+let object = {};
+let array = [];
+let functions = function () {};
