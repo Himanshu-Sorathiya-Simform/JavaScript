cd5bc7c9c05e2dcca3acea50b9631b534477b3f0
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-03-10 11:39:36 +0530

    strings and template literals

---
+++ fundamentals/4. Strings and Template Literals.js
@@ -0,0 +1,20 @@
+// Double quotes
+let name1 = 'Himanshu';
+
+// Single quotes
+let name2 = 'JavaScript';
+
+// Backticks (for template literals, explained below)
+let name3 = `Hello`;
+
+let firstName = 'Himanshu';
+let lastName = 'Sorathiya';
+let age = 21;
+// Normal Concatenation
+let message1 =
+	'My name is ' + firstName + ' ' + lastName + ' and I am ' + age + ' years old.';
+console.log(fullName); // My name is Himanshu Sorathiya and I am 21 years old.
+
+// Template Literals
+let message2 = `My name is ${firstName} ${lastName} and I am ${age} years old.`;
+console.log(fullName); // My name is Himanshu Sorathiya and I am 21 years old.
