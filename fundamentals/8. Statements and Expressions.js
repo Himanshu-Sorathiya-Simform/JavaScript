16765eb73497ba0da4dac141ae20ffb0acd1b31b
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-03-10 12:12:23 +0530

    statements and expressions

---
+++ fundamentals/8. Statements and Expressions.js
@@ -0,0 +1,22 @@
+// Expression
+5 + 3; // Result → 8
+
+10; // Result → 10
+
+('Hello'); // Result → "Hello"
+
+let x = 5;
+x * 2; // Result 10
+
+let result = 5 + 3; // `5 + 3` → expression, `8` gets assigned to `result`
+
+// Statement
+let y = 5; // This is a statement.
+
+if (x > 10) {
+	console.log('Big number');
+} // if is a statement.
+
+for (let i = 0; i < 5; i++) {
+	console.log(i);
+} // for is also a statement.
