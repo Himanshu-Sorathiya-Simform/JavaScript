c54337e2862c9680f6df618c7bf151153819c770
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-03-10 11:44:48 +0530

    truthy and falsy values

---
+++ fundamentals/6. Truthy and Falsy Values.js
@@ -0,0 +1,21 @@
+// There are **exactly 6 falsy values** in JavaScript:
+
+// | Value | Description |
+// | --- | --- |
+// | `false` | Boolean false |
+// | `0` | Number zero |
+// | `""` | Empty string |
+// | `null` | Empty/null value |
+// | `undefined` | Not assigned |
+// | `NaN` | Not-a-Number |
+
+// All other are truthy values
+
+console.log(Boolean(1)); // true
+console.log(Boolean('')); // false
+console.log(Boolean([])); // true
+console.log(Boolean(null)); // false
+console.log(Boolean('Himanshu')); // true
+console.log(Boolean(undefined)); // false
+console.log(Boolean('7')); // true
+console.log(Boolean(false)); // false
