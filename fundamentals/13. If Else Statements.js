132a4850f93c103267575460f0e4a12d1c2f45cc
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-03-10 14:42:25 +0530

    if else statements

---
+++ fundamentals/13. If Else Statements.js
@@ -0,0 +1,11 @@
+let score = 75;
+
+if (score >= 90) {
+	console.log('A grade');
+} else if (score >= 70) {
+	console.log('B grade');
+} else if (score >= 50) {
+	console.log('C grade');
+} else {
+	console.log('Fail');
+}
