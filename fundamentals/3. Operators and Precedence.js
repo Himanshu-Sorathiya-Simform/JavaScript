b6097354b55c3453cef0bf6b9a645dbc14ae2e75
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-03-10 11:35:08 +0530

    operators and precedence

---
+++ fundamentals/3. Operators and Precedence.js
@@ -0,0 +1,27 @@
+// Arithmetic Operators
+5 + 10; // 15
+8 - 3; // 5
+10 * 2; // 20
+10 / 2; // 5
+15 % 4; // 3
+
+// Assignment Operators
+let x = 10;
+x += 5; // x = x + 5 => 15
+x -= 5; // x = x - 5 => 10
+x *= 5; // x = x * 5 => 50
+
+// Comparison Operators
+10 > 5; // true
+10 < 5; // false
+10 >= 10; // true
+10 <= 5; // false
+10 == 10; // true
+10 === '10'; // false
+10 != 10; // false
+10 !== '10'; // true
+
+// Logical Operators
+true && false; // false
+true || false; // true
+!true; // false
