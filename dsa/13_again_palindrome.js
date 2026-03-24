79d9af14ce730c00c1541b74068ec1d99752051e
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-03-24 16:01:26 +0530

    max appearing character

--- dsa/13_again_palindrome.js
+++ dsa/13_again_palindrome.js
@@ -8,11 +8,11 @@ function isPalindrome(str) {
 
 console.log('---------------------------------------');
 console.log('Palindrome');
-// // console.log('Himanshu : ', isPalindrome('Himanshu'));
-// // console.log('    aba : ', isPalindrome('       aba'));
-// // console.log('abba\n\n\n : ', isPalindrome('abba\n\n\n'));
-// // console.log('   krish    : ', isPalindrome('   krish   '));
-// // console.log('k1 r i i r 1k : ', isPalindrome('k1 r i i r 1k'));
+console.log('Himanshu : ', isPalindrome('Himanshu'));
+console.log('    aba : ', isPalindrome('       aba'));
+console.log('abba\n\n\n : ', isPalindrome('abba\n\n\n'));
+console.log('   krish    : ', isPalindrome('   krish   '));
+console.log('k1 r i i r 1k : ', isPalindrome('k1 r i i r 1k'));
 console.log(
 	'\n\n\nH  5 i   m   a   m  i   5H   \n    : ',
 	isPalindrome('\n\n\nH  5 i   m   a   m   i5   H   \n   '),
---
+++ dsa/14_max_frequency.js
@@ -0,0 +1,26 @@
+function maxFrequency(str) {
+	const map = new Map();
+
+	for (const character of str.toLowerCase()) {
+		map.set(character, (map.get(character) || 0) + 1);
+	}
+
+	let ans = '',
+		freq = 0;
+	for (const [key, value] of map) {
+		if (value > freq) {
+			ans = key;
+			freq = value;
+		}
+	}
+
+	return `Character ${ans} appearing ${freq} times`;
+}
+
+console.log('---------------------------------------');
+console.log('Max Frequency of a character');
+
+console.log('Himanshu : ', maxFrequency('Himanshu'));
+console.log('Himanshuuuu : ', maxFrequency('Himanshuuuu'));
+console.log('HHiimanshu : ', maxFrequency('HHiimanshu'));
+console.log('Himaanshu : ', maxFrequency('Himaanshu'));
