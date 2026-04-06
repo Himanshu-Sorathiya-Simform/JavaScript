24c55151ed7020365d18c34a4ac73f08c4c407b0
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-04-06 10:04:15 +0530

    climb staircase

---
+++ dsa/climb.js
@@ -0,0 +1,19 @@
+const ans = [];
+let count = 0;
+
+function climb(num, ans = []) {
+	if (num === 0) {
+		console.log(ans.join(' '));
+		count++;
+	}
+
+	for (let i = 1; i <= num; i++) {
+		ans.push(i);
+		climb(num - i, ans);
+		ans.pop();
+	}
+}
+
+console.log('----------');
+climb(4);
+console.log('COUNT ', count);
